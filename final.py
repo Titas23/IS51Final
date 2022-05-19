8a7e881886ce10916e4743d1e94f9ae37da1c9dc
Andres C. Perez <Titas23@Andress-MacBook-Air.local>
2022-05-19 15:57:10 -0700

    structured english done

---
+++ final.py
@@ -0,0 +1,10 @@
+"""
+In this program we will write a program that is going to calculate the grades of students for their final exam. 
+This program will display the number of grades and also the average grade. 
+By reading the final.txt file where the grades are located. 
+
+It will also include the percentage of grades with an above average score. 
+
+In order to begin the program we will have a main function that will start the program. 
+
+"""
